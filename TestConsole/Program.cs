77230b6e8ffc688062cb4aee74868a758d0cfb9a
Petr Janoušek <pjanousek@lekis.cz>
2023-11-01 15:59:28 +0100

    debugging

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,11 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using NAudio.Wave;
 using RTLSDR;
+using System.IO;
 
-var IQData = File.ReadAllBytes(@"..\..\..\..\Tests\TestData\QI-DATA");
+
+///var IQData = File.ReadAllBytes(@"..\..\..\..\Tests\TestData\QI-DATA");
+var IQData = File.ReadAllBytes(@"C:\temp\RTL-SDR-QI-DATA-2023-10-29-23-04-41.raw");
 var lowPassedData = FMDemodulator.LowPass(IQData, 48000);
 var demodulatedData = FMDemodulator.FMDemodulate(lowPassedData);
 
-File.WriteAllBytes(@"c:\temp\demodulated.bin", demodulatedData);
 
-Console.WriteLine("Hello, World!");
+
+using (var fs = new FileStream(@"c:\temp\demodulated.bin", FileMode.CreateNew))
+{
+
+        foreach (var data in demodulatedData)
+        {
+        var dataToWrite = BitConverter.GetBytes(data);
+            fs.Write(dataToWrite, 0, 2);
+        }
+
+        fs.Flush();
+        fs.Close();
+
+    //fs.Flush();
+    //fs.Close();
+}
+
+
+//File.WriteAllBytes(@"c:\temp\demodulated.bin", demodulatedData);
