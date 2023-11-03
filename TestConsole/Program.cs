fc4c4f0a8bc533894f34698c651c5f87f72a9dad
Petr Janoušek <pjanousek@lekis.cz>
2023-11-03 13:58:51 +0100

    Direct referencing (MonoDevelop buildable)

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,15 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using NAudio.Wave;
+using LoggerService;
 using RTLSDR;
 using System.IO;
+
+var logger = new BasicLoggingService();
+logger.Info("RTL SDR Test Console");
 
 
 ///var IQData = File.ReadAllBytes(@"..\..\..\..\Tests\TestData\QI-DATA");
 var IQData = File.ReadAllBytes(@"C:\temp\RTL-SDR-QI-DATA-2023-10-29-23-04-41.raw");
 var lowPassedData = FMDemodulator.LowPass(IQData, 48000);
 var demodulatedData = FMDemodulator.FMDemodulate(lowPassedData);
-
 
 
 using (var fs = new FileStream(@"c:\temp\demodulated.bin", FileMode.CreateNew))
