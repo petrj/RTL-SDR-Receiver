e73f11d553ccd03d71c7040d46ff23e6c6c1a894
Kirk <kirk@enterprise>
2023-11-03 17:48:27 +0100

    debugging

--- RTLSDRConsole/Program.cs
+++ RTLSDRConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using LoggerService;
 using RTLSDR;
@@ -12,15 +13,29 @@ namespace RTLSDRConsole
             var logger = new BasicLoggingService();
             logger.Info("RTL SDR Test Console");
 
-            var s = System.IO.Path.DirectorySeparatorChar;
-            var IQData = File.ReadAllBytes(@"/temp/iq.data");
-            var lowPassedData = FMDemodulator.LowPass(IQData, 48000);
+            //var s = System.IO.Path.DirectorySeparatorChar;
+            var IQData = File.ReadAllBytes(@"/temp/iq.raw");
+
+            logger.Info($"Total bytes : {IQData.Length}");
+            logger.Info($"Total kbytes: {IQData.Length / 1000}");
+
+            var lowPassedData = FMDemodulator.LowPass(IQData, 96000);
+
+            logger.Info($"Lowpassed data length: {lowPassedData.Length / 1000} kb");
+
             var demodulatedData = FMDemodulator.FMDemodulate(lowPassedData);
 
-            /*
-            using (var fs = new FileStream(@"c:\temp\demodulated.bin", FileMode.CreateNew))
-            {
+            logger.Info($"Demodulated data length: {demodulatedData.Length / 1000} kb");
 
+
+            var targetFileName = "/temp/fm.raw";
+            if (File.Exists(targetFileName))
+            {
+                File.Delete(targetFileName);
+            }
+
+            using (var fs = new FileStream(targetFileName, FileMode.CreateNew))
+            {           
                 foreach (var data in demodulatedData)
                 {
                     var dataToWrite = BitConverter.GetBytes(data);
@@ -29,11 +44,7 @@ namespace RTLSDRConsole
 
                 fs.Flush();
                 fs.Close();
-
-                //fs.Flush();
-                //fs.Close();
             }
-            */
         }
     }
 }
