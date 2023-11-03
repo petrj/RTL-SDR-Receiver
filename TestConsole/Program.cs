8949247a416a4bc79cb0912fe8a8a00e785f6b84
Kirk <kirk@enterprise>
2023-11-03 14:30:07 +0100

    TestConsole modifications for MonoDevelop

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,34 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using LoggerService;
-using RTLSDR;
+﻿using System;
 using System.IO;
+using System.Text;
+using RTLSDR;
+using LoggerService;
 
-var logger = new BasicLoggingService();
-logger.Info("RTL SDR Test Console");
-
-
-///var IQData = File.ReadAllBytes(@"..\..\..\..\Tests\TestData\QI-DATA");
-var IQData = File.ReadAllBytes(@"C:\temp\RTL-SDR-QI-DATA-2023-10-29-23-04-41.raw");
-var lowPassedData = FMDemodulator.LowPass(IQData, 48000);
-var demodulatedData = FMDemodulator.FMDemodulate(lowPassedData);
-
-
-using (var fs = new FileStream(@"c:\temp\demodulated.bin", FileMode.CreateNew))
+namespace TestConsole
 {
-
-        foreach (var data in demodulatedData)
+    public class MainClass
+    {
+        public static void Main(string[] args)
         {
-        var dataToWrite = BitConverter.GetBytes(data);
-            fs.Write(dataToWrite, 0, 2);
+
+            var logger = new BasicLoggingService();
+            logger.Info("RTL SDR Test Console");
+
+
+            ///var IQData = File.ReadAllBytes(@"..\..\..\..\Tests\TestData\QI-DATA");
+            var IQData = File.ReadAllBytes(@"C:\temp\RTL-SDR-QI-DATA-2023-10-29-23-04-41.raw");
+            var lowPassedData = FMDemodulator.LowPass(IQData, 48000);
+            var demodulatedData = FMDemodulator.FMDemodulate(lowPassedData);
+
+
+            using (var fs = new FileStream(@"c:\temp\demodulated.bin", FileMode.CreateNew))
+            {
+
+                foreach (var data in demodulatedData)
+                {
+                    var dataToWrite = BitConverter.GetBytes(data);
+                    fs.Write(dataToWrite, 0, 2);
+                }
+
+                fs.Flush();
+                fs.Close();
+
+                //fs.Flush();
+                //fs.Close();
+            }
         }
-
-        fs.Flush();
-        fs.Close();
-
-    //fs.Flush();
-    //fs.Close();
+    }
 }
 
 
-//File.WriteAllBytes(@"c:\temp\demodulated.bin", demodulatedData);
+
