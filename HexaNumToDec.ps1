f44a9ac3ac0589c3efe02426f9e9d109733f511c
Petr Janoušek <pjanousek@lekis.cz>
2023-11-10 10:30:26 +0100

    PS code for converting hexa numbers

---
+++ HexaNumToDec.ps1
@@ -0,0 +1,52 @@
+﻿function ConvertFrom-HexaStringNum
+{
+    param
+    (
+        [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
+        [string]$HexaStringNumber
+    )
+    process
+    {
+        $num = 0
+
+        if ($HexaStringNumber.StartsWith("0x"))
+        {
+            $HexaStringNumber = $HexaStringNumber.Substring(2)
+        }
+
+        $nums = @()
+
+        while ($HexaStringNumber.Length -ge 2)
+        {
+            $hexaNum = $HexaStringNumber.Substring(0,2)
+            $dexNum = [convert]::toint64($hexaNum,16)
+            $nums += $dexNum        
+            $HexaStringNumber = $HexaStringNumber.Substring(2) 
+        }
+
+        [Array]::Reverse($nums)
+
+        return [System.BitConverter]::ToUInt32($nums, 0)
+    }
+}
+
+function ConvertTo-HexaStringNum
+{
+    param
+    (
+        [Parameter(Mandatory = $true,ValueFromPipeline = $true)]
+        [uint32]$number
+    )
+    process
+    {  
+        $bytes = [System.BitConverter]::GetBytes($number)
+        [Array]::Reverse($bytes)
+        return ("0x" +[System.BitConverter]::ToString($bytes).Replace("-",""))
+    }
+}
+
+
+"0x010636d658" | ConvertFrom-HexaStringNum
+#1024000 | ConvertTo-HexaStringNum
+
+
