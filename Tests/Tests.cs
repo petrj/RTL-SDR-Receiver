6bbb8dd68949b680799af0a4075f0f9abf634d4d
petrj <petrjanousek.net@gmail.com>
2024-02-29 23:15:35 +0100

    FEC test

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -11,7 +11,9 @@ namespace Tests
         public void TestFEC()
         {
             var testData = File.ReadAllBytes($"TestData{Path.DirectorySeparatorChar}RSTestData.bin");
-            var corr_count = new ReedSolomonCodecControlBlock(8, 0x11D, 0, 1, 10, 135);
+            var rs = new ReedSolomonCodecControlBlock(8, 0x11D, 0, 1, 10, 135);
+            var corrPos = new int[10];
+            var corr_count = rs.decode_rs_char(testData, corrPos, 0);
             Assert.AreEqual(5, corr_count);
         }
     }
