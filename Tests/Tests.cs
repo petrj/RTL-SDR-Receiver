647f312dd70a914481e2c32ffe28c4e087482ee9
petrj <petrjanousek.net@gmail.com>
2024-02-29 22:25:49 +0100

    FEC test

---
+++ Tests/Tests.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RTLSDR.DAB;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestFEC()
+        {
+            var testData = File.ReadAllBytes($"TestData{Path.DirectorySeparatorChar}RSTestData.bin");
+            var corr_count = new ReedSolomonCodecControlBlock(8, 0x11D, 0, 1, 10, 135);
+            Assert.AreEqual(5, corr_count);
+        }
+    }
+}
