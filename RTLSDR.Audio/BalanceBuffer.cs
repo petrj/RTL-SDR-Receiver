807646234f9e8090a4fd93ccc4477209d583ed69
petrj <petrjanousek.et@gmail.com>
2024-09-28 00:00:30 +0200

    preliminaryOutputBufferBytes

--- RTLSDR.Audio/BalanceBuffer.cs
+++ RTLSDR.Audio/BalanceBuffer.cs
@@ -18,6 +18,11 @@ public class BalanceBuffer
     private const int MinThreadNoDataMSDelay = 25;
     private const int CycleMSDelay = 100; // 10x per sec
 
+    private const int PreliminaryOutputBufferMS = 1000;
+
+    private long _pcmBytesInput = 0;
+    private long _pcmBytesOutput = 0;
+
     private ILoggingService _loggingService;
 
     private AudioDataDescription _audioDescription;
@@ -71,11 +76,12 @@ public class BalanceBuffer
         List<byte> _audioBuffer = new List<byte>();
         byte[] data = null;
 
+        var loopStartTime = DateTime.Now;
+
         try
         {
             while (_running)
             {
-                
                 cycleStartTime = DateTime.Now; // start of next cycle
 
                 var totalBytesRead = 0;
@@ -90,57 +96,60 @@ public class BalanceBuffer
                     {
                         totalBytesRead+=data.Length;
                         _audioBuffer.AddRange(data);
+                        _pcmBytesInput += data.Length;
                     } 
 
                     Thread.Sleep(MinThreadNoDataMSDelay);
                 }
 
+                //if ((DateTime.Now-loopStartTime).TotalMilliseconds<PreliminaryOutputBufferMS)
+                //{
+                //    _loggingService.Debug($" -->|OOO|--> filling buffer");
+                //    continue;
+                //}
+
                 var bytesPerSample = (_audioDescription.BitsPerSample/8)*_audioDescription.Channels;
                 var bytesPerSec = _audioDescription.SampleRate*bytesPerSample;
                 var secsFromLastCycle = (DateTime.Now - cycleStartTime).TotalSeconds;                
 
-                var bytesFromLastCycle = (Convert.ToInt32(secsFromLastCycle * bytesPerSec)/bytesPerSample)*(bytesPerSample)-bytesPerSample;
+                var cycleBytes = (Convert.ToInt32(secsFromLastCycle * bytesPerSec)/bytesPerSample)*(bytesPerSample)-bytesPerSample;
 
-                bytesFromLastCycle += 2*bytesFromLastCycle;
+                //cycleBytes += cycleBytes;
+
+                var preliminaryOutputBufferBytes = bytesPerSec*PreliminaryOutputBufferMS/1000;
+
+                if ((_pcmBytesInput-_pcmBytesOutput)<preliminaryOutputBufferBytes)
+                {
+                    _loggingService.Debug($" -->|OOO|--> buffering, missing {(preliminaryOutputBufferBytes - cycleBytes)/1000} kB ");
+                    cycleBytes = preliminaryOutputBufferBytes - cycleBytes;
+                }
 
                 // deque bytesFromLastCycle bytes:
-                if ((bytesFromLastCycle > 0 ) && (bytesFromLastCycle <= _audioBuffer.Count))
+                if ((cycleBytes > 0 ) && (_audioBuffer.Count>0))
                 {
-                    _loggingService.Debug($"Dequeue {bytesFromLastCycle} bytes");
-                    var thisCycleBytes = _audioBuffer.GetRange(0, Convert.ToInt32(bytesFromLastCycle));
-                    _audioBuffer.RemoveRange(0, Convert.ToInt32(bytesFromLastCycle));
-                    
+                    if (cycleBytes > _audioBuffer.Count)
+                    {
+                        cycleBytes = _audioBuffer.Count;
+                    }
+
+                    //_loggingService.Debug($"Dequeue {bytesFromLastCycle} bytes");
+                    var thisCycleBytes = _audioBuffer.GetRange(0, Convert.ToInt32(cycleBytes));
+                    _audioBuffer.RemoveRange(0, Convert.ToInt32(cycleBytes));
+
                     _actionPlay(thisCycleBytes.ToArray());
+                    _pcmBytesOutput += cycleBytes;
                 } else
                 {
                   // no data in buffer! Notify slow CPU
-                    //_loggingService.Debug($"No data in buffer");                  
+                    _loggingService.Debug($"No data in buffer");
                 }
 
                 if ((DateTime.Now-lastNotifiTime).TotalSeconds>2)
                 {
-                    _loggingService.Debug($"iiii  <{_queue.Count}> ==>{totalBytesRead} B  <{_audioBuffer.Count}> ==> {bytesFromLastCycle} B");
+                    //_loggingService.Debug($"iiii  <{_queue.Count}> ==>{totalBytesRead} B  <{_audioBuffer.Count}> ==> {bytesFromLastCycle} B");
+                    _loggingService.Debug($" -->|OOO|--> {(_pcmBytesInput)/1000} -> {(_pcmBytesOutput)/1000}        kB Δ {(_pcmBytesInput-_pcmBytesOutput)/1000} kB       ({_audioBuffer.Count/1000} kB)");
                     lastNotifiTime = DateTime.Now;
                 }
-
-                            
-                /*
-                if (_action != null && data != null)
-                {
-                    var startTime = DateTime.Now;
-
-                    _action(data);
-
-                    _workingTimeMS += (DateTime.Now - startTime).TotalMilliseconds;
-                } else
-                {
-                    Thread.Sleep(_actionMSDelay);
-                }
-                */
-
-                
-
-                
             }
         }
         catch (Exception ex)
