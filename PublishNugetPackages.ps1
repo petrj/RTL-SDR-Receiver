11f17b92471589759254d90f0ae166caa6ea9edd
petrj <petrjanousek.net@gmail.com>
2025-10-22 22:24:58 +0200

    nugets

---
+++ PublishNugetPackages.ps1
@@ -0,0 +1,34 @@
+﻿cd $PSScriptRoot
+
+$packageVersion = "1.0.0"
+
+Write-Host "enter github access token: " -NoNewline
+$token = Read-Host
+
+
+function Publish-Project
+{    
+    [CmdletBinding()]
+    param (
+        [string]$ProjectName,
+        [string]$PackageVersion,
+        [string]$Token,
+        [string]$PSScriptRoot
+    )
+    process 
+    {
+        $ProjectPath = Join-Path $PSScriptRoot -ChildPath "$ProjectName\$ProjectName.csproj"
+
+        dotnet build $ProjectPath -c Release /p:PackageVersion=$packageVersion
+        
+        $fName = Join-Path $PSScriptRoot -ChildPath "$ProjectName\bin\release\$ProjectName.$PackageVersion.nupkg"
+        dotnet nuget push $fName -k $Token --source "github" # --skip-duplicate
+    }
+}
+
+# RTLSDR.Common
+
+#Publish-Project -ProjectName "RTLSDR.Common" -PackageVersion $packageVersion -PSScriptRoot $PSScriptRoot -Token $token
+Publish-Project -ProjectName "RTLSDR" -PackageVersion $packageVersion -PSScriptRoot $PSScriptRoot -Token $token
+
+
