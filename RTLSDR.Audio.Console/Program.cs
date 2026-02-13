ced408fd66fd5a601fef553c8ba8accfc22d149b
petrj <petrjanousek.et@gmail.com>
2026-02-13 22:58:22 +0100

    sound reinit test

--- RTLSDR.Audio.Console/Program.cs
+++ RTLSDR.Audio.Console/Program.cs
@@ -2,6 +2,10 @@
 using RTLSDR.Audio;
 using RTLSDR.Common;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+CancellationTokenSource cancellationToken = new CancellationTokenSource();
 
 IRawAudioPlayer rawAudioPlayer;
 ILoggingService loggingService = new BasicLoggingService();
@@ -12,8 +16,7 @@ if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 }
 else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
 {
-    //rawAudioPlayer = new AlsaSoundAudioPlayer();                     // Linux only
-    rawAudioPlayer = new VLCSoundAudioPlayer();                     // Linux + Windows
+    rawAudioPlayer = new AlsaSoundAudioPlayer();                     // Linux only
 }
 else
 {
@@ -21,55 +24,76 @@ else
     rawAudioPlayer = new NoAudioRawAudioPlayer();                    // dummy interface
 }
 
-rawAudioPlayer.Init(new AudioDataDescription()
+//rawAudioPlayer = new VLCSoundAudioPlayer();                     // Linux + Windows
+
+
+void ReadFile(string fName, CancellationTokenSource cancellationToken)
+{
+    Task.Run(() =>
+    {
+        try
+        {
+            using (var fs = new FileStream(fName, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[8000];
+                int bytesRead;
+                while (!cancellationToken.IsCancellationRequested && ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0))
+                {
+                    if (bytesRead < buffer.Length)
+                    {
+                        // If we read less than the buffer size, we need to trim the buffer
+                        byte[] trimmedBuffer = new byte[bytesRead];
+                        Array.Copy(buffer, trimmedBuffer, bytesRead);
+                        rawAudioPlayer.AddPCM(trimmedBuffer);
+                    }
+                    else
+                    {
+                        rawAudioPlayer.AddPCM(buffer);
+                    }
+
+                    Thread.Sleep(50);
+                }
+            }
+
+            Console.WriteLine($"File processed");
+
+        } catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading audio file: {ex.Message}");
+        }
+    });
+
+}
+
+var folder = AppContext.BaseDirectory;
+var fName = Path.Join(folder,"samples","16bLE44st.wav");
+
+var desc44 = new AudioDataDescription()
 {
  BitsPerSample = 16,
   Channels = 2,
    SampleRate = 44100
-}, loggingService);
+};
 
 
-bool quit = false;
-
-Task.Run(() =>
-{
-    try
-    {
-        using (var fs = new FileStream("samples" + System.IO.Path.DirectorySeparatorChar + "16bLE44st.wav", FileMode.Open, FileAccess.Read))
-        {
-            byte[] buffer = new byte[8000];
-            int bytesRead;
-            while (!quit && ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0))
-            {
-                if (bytesRead < buffer.Length)
-                {
-                    // If we read less than the buffer size, we need to trim the buffer
-                    byte[] trimmedBuffer = new byte[bytesRead];
-                    Array.Copy(buffer, trimmedBuffer, bytesRead);
-                    rawAudioPlayer.AddPCM(trimmedBuffer);
-                }
-                else
-                {
-                    rawAudioPlayer.AddPCM(buffer);
-                }
-
-                Thread.Sleep(50);
-            }
-        }
-
-        Console.WriteLine($"File processed");
-
-    } catch (Exception ex)
-    {
-        Console.WriteLine($"Error reading audio file: {ex.Message}");
-    }
-});
-
-
+rawAudioPlayer.Init(desc44, loggingService);
 rawAudioPlayer.Play();
+ReadFile(fName,cancellationToken);
 
 Console.WriteLine("Press Enter...");
 Console.ReadLine();
 
 rawAudioPlayer.Stop();
-quit = true;
+cancellationToken.Cancel();
+
+// reinit test
+
+rawAudioPlayer.Init(desc44, loggingService);
+rawAudioPlayer.Play();
+ReadFile(fName,cancellationToken);
+
+Console.WriteLine("Press Enter...");
+Console.ReadLine();
+
+rawAudioPlayer.Stop();
+cancellationToken.Cancel();
