8fa6e24af059848b5149d4aaf66116855b97662b
petrj <petrjanousek.net@gmail.com>
2026-02-14 12:08:18 +0100

    96 khz audio test

--- RTLSDR.Audio.Console/Program.cs
+++ RTLSDR.Audio.Console/Program.cs
@@ -10,6 +10,7 @@ CancellationTokenSource cancellationToken = new CancellationTokenSource();
 IRawAudioPlayer rawAudioPlayer;
 ILoggingService loggingService = new BasicLoggingService();
 
+
 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 {
     rawAudioPlayer = new NAudioRawAudioPlayer(loggingService);       // Windows only
@@ -24,12 +25,14 @@ else
     rawAudioPlayer = new NoAudioRawAudioPlayer();                    // dummy interface
 }
 
+
 //rawAudioPlayer = new VLCSoundAudioPlayer();                     // Linux + Windows
 
 EventHandler Finished = null;
 
 void ReadFile(IRawAudioPlayer rawAudioPlayer, string fName, CancellationTokenSource cancellationToken)
 {
+    //Console.WriteLine($"Starting to read file: {fName}");
     Task.Run(() =>
     {
         try
@@ -52,7 +55,7 @@ void ReadFile(IRawAudioPlayer rawAudioPlayer, string fName, CancellationTokenSou
                         rawAudioPlayer.AddPCM(buffer);
                     }
 
-                    Thread.Sleep(50);
+                    Thread.Sleep(25);
                 }
             }
 
@@ -72,7 +75,8 @@ void ReadFile(IRawAudioPlayer rawAudioPlayer, string fName, CancellationTokenSou
 }
 
 var folder = AppContext.BaseDirectory;
-var fName = Path.Join(folder,"samples","16bLE44st.wav");
+var fName44 = Path.Join(folder,"samples","16bLE44st.wav");
+var fName96 = Path.Join(folder,"samples","16bLE96st.wav");
 
 var desc44 = new AudioDataDescription()
 {
@@ -81,8 +85,14 @@ var desc44 = new AudioDataDescription()
    SampleRate = 44100
 };
 
+var desc96 = new AudioDataDescription()
+{
+ BitsPerSample = 16,
+  Channels = 2,
+   SampleRate = 96000
+};
 
-rawAudioPlayer.Init(desc44, loggingService);
+rawAudioPlayer.Init(desc96, loggingService);
 rawAudioPlayer.Play();
 Finished += (s, e) =>
 {
@@ -93,10 +103,10 @@ Finished += (s, e) =>
     rawAudioPlayer.Init(desc44, loggingService);
     rawAudioPlayer.Play();
 
-    ReadFile(rawAudioPlayer,fName,cancellationToken);
+    ReadFile(rawAudioPlayer,fName44,cancellationToken);
 };
 
-ReadFile(rawAudioPlayer,fName,cancellationToken);
+ReadFile(rawAudioPlayer,fName96,cancellationToken);
 
 Console.WriteLine("Press Enter to stop playback...");
 Console.ReadLine();
