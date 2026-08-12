733711791a3e08f90a1c3cab15e1f2195cd2dc9d
petrj <petrjanousek.net@gmail.com>
2026-08-12 22:55:13 +0200

    downgrading libvlcSharp to 3.9.4

--- PublishNugetPackages.ps1
+++ PublishNugetPackages.ps1
@@ -6,6 +6,6 @@ $token = Get-SecureStringFromUserInput -Message "Enter github access token:" -En
 
 #Publish-Nuget -ProjectName "RTLSDR" -PackageVersion "1.5.1" -SolutionPath $PSScriptRoot -Token $token
 #Publish-Nuget -ProjectName "RTLSDR.Common" -PackageVersion "1.5.8" -SolutionPath $PSScriptRoot -Token $token
-#Publish-Nuget -ProjectName "RTLSDR.Audio" -PackageVersion "1.5.1" -SolutionPath $PSScriptRoot -Token $token
+Publish-Nuget -ProjectName "RTLSDR.Audio" -PackageVersion "1.5.2" -SolutionPath $PSScriptRoot -Token $token
 #Publish-Nuget -ProjectName "RTLSDR.FM" -PackageVersion "1.5.2" -SolutionPath $PSScriptRoot -Token $token
-Publish-Nuget -ProjectName "RTLSDR.DAB" -PackageVersion "1.6.3" -SolutionPath $PSScriptRoot -Token $token
+#Publish-Nuget -ProjectName "RTLSDR.DAB" -PackageVersion "1.6.3" -SolutionPath $PSScriptRoot -Token $token
